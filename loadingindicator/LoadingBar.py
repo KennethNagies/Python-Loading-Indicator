8627748220e6ee9e940b1d8e78a4848ce0939d53
Kenneth Nagies <kennethnagies@gmail.com>
2021-07-18 09:48:23 -0700

    Add Loading Percentage Indicator To Loading Bar
    This adds a simple loading percentage indicator header to the right side
    of the loading bar

--- loadingindicator/LoadingBar.py
+++ loadingindicator/LoadingBar.py
@@ -7,7 +7,6 @@ class LoadingBar(LoadingIndicator):
         self._current_progress = 0
         self._printed = False
 
-
     def draw(self):
         sys.stdout.write(str(self))
         sys.stdout.flush()
@@ -15,25 +14,38 @@ class LoadingBar(LoadingIndicator):
     def set_progress(self, progress):
         self._current_progress = min(self._max_progress, progress)
 
-    def __str__(self):
-        terminal_width = os.get_terminal_size()[0]
+    def get_bar(self, terminal_width, loaded_percent):
         bar_length = terminal_width - 2
         if (bar_length < 0):
             return "X"
         elif (bar_length == 0):
             return "[]"
+        filled_bar_length = int(bar_length * loaded_percent)
+        empty_bar_length = bar_length - filled_bar_length
+        return f"[{'#' * filled_bar_length}{' ' * empty_bar_length}]"
+
+    def get_header(self, terminal_width, loaded_percent):
+        if (terminal_width < 4):
+            return ""
+        return f"{' ' * (terminal_width - 4)}{int(loaded_percent * 100):>3}%"
+
+    def __str__(self):
+        terminal_width = os.get_terminal_size()[0]
         if (self._current_progress == 0):
             loaded_percent = 0
         else:
             loaded_percent = self._current_progress / self._max_progress
-        filled_bar_length = int(bar_length * loaded_percent)
-        empty_bar_length = bar_length - filled_bar_length
         if (self._printed):
-            bar = "\r"
+            indicator = "\033[F"
         else:
-            bar = ""
-        bar = bar + f"[{'#' * filled_bar_length}{' ' * empty_bar_length}]"
+            indicator = ""
+        header = self.get_header(terminal_width, loaded_percent)
+        bar = self.get_bar(terminal_width, loaded_percent)
+        if (len(header) > 0):
+            indicator += f"{header}{os.linesep}{bar}"
+        else:
+            indicator += bar
         self._printed = True
         if (loaded_percent == 1):
-            bar += os.linesep
-        return bar
+            indicator += os.linesep
+        return indicator
